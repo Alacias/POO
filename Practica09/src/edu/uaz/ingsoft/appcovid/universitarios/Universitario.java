cafb5949f098500059ebeda54df03cccdd55a7fd
Alacias <aldomaciasaleman@gmail.com>
2021-10-08 11:31:26 -0500

    Update Universitario.java

--- Practica09/src/edu/uaz/ingsoft/appcovid/universitarios/Universitario.java
+++ Practica09/src/edu/uaz/ingsoft/appcovid/universitarios/Universitario.java
@@ -3,8 +3,8 @@ package Practica09.src.edu.uaz.ingsoft.appcovid.universitarios;
 public class Universitario {
     private String nombre;
 
-    public String getNombre() {
-        return nombre;
+    Universitario(String nombre) {
+        this.nombre = nombre;
     }
 
     public boolean equals(Object o) {
