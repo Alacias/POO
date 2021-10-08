c887cb3e19904df5f79793fc53ab0c33704566dd
Alacias <aldomaciasaleman@gmail.com>
2021-10-08 09:49:15 -0500

    Update Maestro.java

---
+++ Practica09/src/edu/uaz/ingsoft/appcovid/universitarios/maestros/Maestro.java
@@ -0,0 +1,12 @@
+package Practica09.src.edu.uaz.ingsoft.appcovid.universitarios.maestros;
+import Practica09.src.edu.uaz.ingsoft.appcovid.universitarios.Universitario;
+
+class Maestro extends Universitario {
+    private float sueldo;
+
+    while (sueldo >= 100 && sueldo <= 20000) {
+        
+    }
+    
+}
+
