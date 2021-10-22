2f112375f33f26363c4a2bd7f13e196916d5e789
Alacias <aldomaciasaleman@gmail.com>
2021-10-22 08:29:36 -0500

    Practica06 Paso 03

---
+++ Practica06/src/mx/com/softgame/poo1game/personajes/Personaje.java
@@ -0,0 +1,37 @@
+package mx.com.softgame.poo1game.personajes;
+
+public class Personaje {
+    private String nombre;
+    private int edad;
+
+    public Personaje(String nombre) {
+        this.nombre = nombre;
+        edad = 0;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public boolean setEdad(int edad) {
+        if (edad > 0 && edad < 120) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre.length > 3 && nombre.length < 20) {
+            this.nombre = nombre;
+        }
+    }
+
+    public void saludar() {
+        System.out.println("Hola Alumno de POO " + nombre);
+    }
+
+}
